4e8c2bcd760e9ad47d7ea447648dea524e0747fe
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-19 01:07:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let childElt = document.getElementById("level");
+let cnt=0;
+while(childElt!=null){
+    cnt++;
+    childElt = childElt.parentElement;
+}
+
+alert(`The level of the element is: ${cnt}`);
